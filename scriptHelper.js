b9394786f3e2387eecd134d8a1ef1f196a0cde82
Elizabeth Karr <e.karr93@icloud.com>
2023-04-18 15:37:30 -0500

    fill in validateInput function

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,7 +17,12 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+   if (testInput === "") {
+    return "Empty";
+   } else if (isNaN(testInput)) {
+    return "Not a Number";
+   }
+   return "Is a Number";
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
